5d12b50bc99a18407e65916ca67cc47f67bd7c9b
Cody L Hall <chal032687@gmail.com>
2024-03-01 17:48:37 -0500

    removed dev integration and added lottie integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import tailwindConfigViewer from "astro-tailwind-config-viewer";
+import lottie from "astro-integration-lottie";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    configFile: './astro.config.mjs',
-  }), 
-    tailwindConfigViewer({
-      configFile: './astro.config.mjs',
-      endpoint: '/_tailwind',
-      overlayMode: "embed"
-    })]
+  integrations: [tailwind(), lottie()]
 });
