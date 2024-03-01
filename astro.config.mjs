cc1d2966245bbba46bf53c227fcefcba08e270aa
Cody L Hall <chal032687@gmail.com>
2024-03-01 02:11:53 -0500

    good questtion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import tailwindConfigViewer from "astro-tailwind-config-viewer";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind({
+    configFile: './astro.config.mjs',
+  }), 
+    tailwindConfigViewer({
+      configFile: './astro.config.mjs',
+      endpoint: '/_tailwind',
+      overlayMode: "embed"
+    })]
 });
