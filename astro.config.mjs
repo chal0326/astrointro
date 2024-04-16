9de46ea7246d0f811d548a003d184415a7f0e514
Cody L Hall <chal032687@gmail.com>
2024-04-16 02:55:32 -0400

    removed chart cards and general maintenance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import lottie from "astro-integration-lottie";
+import { defineConfig } from "astro/config";
+import astroDevToolBreakpoints from "astro-devtool-breakpoints";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lottie()],
+  integrations: [tailwind(), lottie(), astroDevToolBreakpoints()],
 });
